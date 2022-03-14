58a22471d3271b584ed9dadf92870827e30acd0b
trangiang911 <tbgiang191199@gmail.com>
2022-03-14 16:22:18 +0700

    up

---
+++ case_study_module2/src/services/IFacilityService.java
@@ -0,0 +1,4 @@
+package services.interface_service;
+
+public interface IFacilityService {
+}
---
+++ src/ss19_string_regex/bai_tap/CheckValidClassName.java
@@ -0,0 +1,20 @@
+package ss19_string_regex.bai_tap;
+
+import java.util.Scanner;
+
+public class CheckValidClassName {
+    public static boolean checkValidClassName(String className) {
+        String regex = "^([ACP])\\d{4}([GHIKLM])$";
+        return className.matches(regex);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String className;
+        do {
+            System.out.println("enter class name");
+            className = scanner.nextLine();
+            CheckValidClassName.checkValidClassName(className);
+        }while (!CheckValidClassName.checkValidClassName(className));
+        System.out.println("name ok !");
+    }
+}
